c95fcf5eeab94e6d2d531047c15e4ccb95c51f35
csboy2345 <116421872+csboy2345@users.noreply.github.com>
2022-10-22 12:01:27 +0530

    Update Axis_Command.m

--- Axis_Command.m
+++ Axis_Command.m
@@ -1,7 +1,7 @@
-x = -5 : 0.1 : 10;
+x = -5 : 0.1 : 10; %Dimensions
 y = 3 .* x .* x + 2 .* x + 5;
 axis([-5 5 0 20]);
 plot(x, y);
-xlabel('Values');
-ylabel('3x2 + 2x + 5');
+xlabel('Values'); %to draw x axsis
+ylabel('3x2 + 2x + 5'); % to draw y axsis
 grid on;
